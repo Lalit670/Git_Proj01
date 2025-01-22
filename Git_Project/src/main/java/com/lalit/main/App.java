5539d18141f666308cd70cbd39d0e7b68c1563ee
lalit <lalitbhagatbgt@gmail.com>
2025-01-22 17:56:16 +0530

    initial Commit

---
+++ Git_Project/src/main/java/com/lalit/main/App.java
@@ -0,0 +1,11 @@
+package com.lalit.main;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World! with mukul");
+    }
+}
